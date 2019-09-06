87bd44187aaeef86cfca38b258120c9310a681dc
Matt Oestreich <matthewpoestreich@gmail.com>
2019-09-06 09:06:02 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,21 +2,12 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import AppScrollTrigger from './AppScrollTrigger'
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            name: 'React'
-        };
-    }
-
-    render() {
-        return (
-            <div>
-                <AppScrollTrigger />
-            </div>
-        );
-    }
+function App() {
+    return (
+        <div>
+            <AppScrollTrigger />
+        </div>
+    );    
 }
 
 render(<App />, document.getElementById('root'));
